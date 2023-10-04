7202f061f0010f6ee7c1ef21f775a9938abe5cdd
aryan chaudhary <114851758+aryanch12@users.noreply.github.com>
2023-10-04 15:59:22 +0530

    Time: 4 ms (38.06%), Space: 7.5 MB (56.70%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //POWER SET - STRIVER
+    vector<vector<int>> subsets(vector<int>& nums){
+        vector<vector<int>> TotalSubsets;
+        int n=nums.size();
+        for(int num=0;num<=pow(2,n)-1;num++){
+            vector<int> sub;
+            for(int i=0;i<=n-1;i++){
+                if(num&(1<<i)){
+                    sub.push_back(nums[i]);
+                }
+            }
+            TotalSubsets.push_back(sub);
+        }
+        return TotalSubsets;
+    }
+};
