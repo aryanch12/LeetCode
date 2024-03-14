5e4cf7eb60ebcb5f994cc4b1022937a6b994b8b1
aryan chaudhary <114851758+aryanch12@users.noreply.github.com>
2024-03-15 01:05:51 +0530

    Time: 20 ms (27.77%), Space: 23.7 MB (5.41%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& arr) {
-        int n = arr.size(); // taking the size of the array 
-        
-        unordered_map<int, int> mp; // unordered map to store the frequency
-        
-        // storing frequency in the map
-        for(int i = 0; i < n; i++)
-        {
-            mp[arr[i]]++;
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(int i=0; i<n;i++){
+            mp[nums[i]]++;
         }
-        
-        int ans; // variable to store our answer
-        for(auto x: mp) // traverse from the map
-        {
-            if(x.second == 1) //if frequency of any elemennt is 1
-            {
-                ans = x.first; // store in our answer
-                break; // break the loop, as we got our answer now
-            }
-        }
-        
-        return ans; // return ans
+        for(auto i:mp)
+		   if(i.second==1)
+			   return i.first;
+	   return -1;
     }
 };
