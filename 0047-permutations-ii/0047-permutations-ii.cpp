9ab9f41e10f97f096fdb8620df41210c24f3f57b
aryan chaudhary <114851758+aryanch12@users.noreply.github.com>
2024-04-12 21:23:48 +0530

    Time: 4 ms (77.82%), Space: 11.4 MB (65.73%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> result;
+    void solve(unordered_map<int,int>&mp,vector<int> &temp){
+        if(temp.size()==n){
+            result.push_back(temp);//o(n)
+            return;
+        }
+        
+        for(auto&[num,count]:mp){
+            if(count==0) continue;
+            
+            temp.push_back(num);
+            mp[num]--;
+            
+            solve(mp,temp);
+            
+            temp.pop_back();
+            mp[num]++;
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n=nums.size();
+        unordered_map<int,int> mp;
+        for(int &i:nums){
+            mp[i]++;
+        }
+        
+        vector<int> temp;
+        solve(mp,temp);
+        return result;
+        
+    }
+};
